d6379bc57f059854db8c9990e92e135f0a8cc375
João Lucas Nascimento Andrade <jlnascimentoandrade@gmail.com>
2022-03-13 19:58:15 -0300

    axios

--- financeapp/src/Components/TrasanctiosTable/index.tsx
+++ financeapp/src/Components/TrasanctiosTable/index.tsx
@@ -1,43 +1,45 @@
-import { Container } from "./style";
+import {useEffect} from 'react';
+import {api} from '../../services/api';
+import {Container} from './style';
 
+export function TrasanctionTable() {
+  useEffect(() => {
+    api.get('transactions')
+    .then((response) => console.log(response.data));
+  }, []);
 
-export function TrasanctionTable(){
-    return(
-        <Container>
-
-            <table>
-            <thead>
-                <tr>
-                <th>Título</th>
-                <th>Valor</th>
-                <th>categoria</th>
-                <th>Data</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Desenvolvimento de Site</td>
-                    <td className = "deposit">R$1.000,00</td>
-                    <td>Trablaho freelancer</td>
-                    <td>10/03/2022</td>
-                </tr>
-                <tr>
-                    <td>Salário</td>
-                    <td className = "deposit">R$4.000,00</td>
-                    <td>Tarbalho</td>
-                    <td>05/03/2022</td>
-                </tr>
-                <tr>
-                    <td>Aluguel</td>
-                    <td className= "withdraw">- R$2.000,00</td>
-                    <td>Casa</td>
-                    <td>13/03/2022</td>
-                </tr>
-            </tbody>
-
-            </table>
-
-        </Container>
-
-    );
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de Site</td>
+            <td className="deposit">R$1.000,00</td>
+            <td>Trablaho freelancer</td>
+            <td>10/03/2022</td>
+          </tr>
+          <tr>
+            <td>Salário</td>
+            <td className="deposit">R$4.000,00</td>
+            <td>Tarbalho</td>
+            <td>05/03/2022</td>
+          </tr>
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">- R$2.000,00</td>
+            <td>Casa</td>
+            <td>13/03/2022</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  )
 }
