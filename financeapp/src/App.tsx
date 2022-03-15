b4f44ad8b937c12d1efc514bc36da0f20feaa9da
João Lucas Nascimento Andrade <jlnascimentoandrade@gmail.com>
2022-03-15 20:20:46 -0300

    modal

--- financeapp/src/App.tsx
+++ financeapp/src/App.tsx
@@ -1,13 +1,30 @@
 import { Dashboard } from "./Components/Dashboard";
 import { Header } from "./Components/Header";
 import { GlobalStyle } from "./Styles/global";
+import { useState } from "react";
+import { NewTransactionModal } from "./Components/Modal";
+import Modal from 'react-modal';
 
+Modal.setAppElement("#root");
 
 export function App() {
+  const [isNewTransactionModalopen, setNewTransactionModal] = useState(false);
+
+  function handleOpenNewTransactionModal(){
+      setNewTransactionModal(true);
+  }
+
+  function handleCloseNewTransactionModal(){
+      setNewTransactionModal(false);
+  }
   return (
     <>
-      <Header/>
-      <Dashboard/>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+      <Dashboard/>      
+      <NewTransactionModal
+       isOpen={isNewTransactionModalopen}
+       onRequestClose={handleCloseNewTransactionModal}
+       />
       <GlobalStyle/>
     </>
   );
